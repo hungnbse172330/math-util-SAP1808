d2eded0ba8b9a8fbc1c364ad686434f2a8ef5c28
hungnbse172330 <hungnbse172330@fpt.edu.vn>
2024-06-04 11:14:22 +0700

    first upload

---
+++ src/main/java/com/hungnb/mathutil/main/MathUtilSAP1808.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hungnb.mathutil.main;
+
+/**
+ *
+ * @author SmileLord
+ */
+public class MathUtilSAP1808 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
